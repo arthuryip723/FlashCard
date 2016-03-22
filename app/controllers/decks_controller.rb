01d29cedc44e2dbcdaec10c3061bb5e6afdd73ad
Arthur Yip <arthuryip723@gmail.com>
2016-03-22 00:21:42 -0400

    end of mar 22

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -6,4 +6,21 @@ class DecksController < ApplicationController
   def show
   	@deck = Deck.find(params[:id])
   end
+
+  # This method is used for polling the card
+  # It should return a card tht needed to be memorized immediately
+  def poll
+  	deck = Deck.find(params[:id])
+  	# Then find the card
+  	# FIXME fix it later, now focus on the valid functionality
+  	@card = deck.cards.find_by(state: 'AGAIN')
+  	if !@card
+  		@card = deck.cards.find_by(state: 'GOOD')
+  	end
+  	if !@card
+  		@card = deck.cards.find_by(state: 'EASY')
+  	end
+  	# Now I've got the card
+  	# Find the deck with the most urgent cards, find the most recent date of modification
+  end
 end
